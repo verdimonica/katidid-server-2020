fa39c114af5d38864ffc1086d3dc666ddeca6397
Monica Verdi <monica.verdi@gmail.com>
2020-12-09 13:01:44 +0100

    add get all and get childById routes

--- routes/child.router.js
+++ routes/child.router.js
@@ -7,16 +7,51 @@ const Child = require ('./../models/child.model');
 router.post('/child', (req, res, next) => {
     const {name, image, age, parents, parents_mail, parents_phone} = req.body;
     Child.create({name, image, age, parents, parents_mail, parents_phone, events:[]} )
-        .then( (createdChild) => {
+        .then((createdChild) =>{
             res
-            .status(201)
-            .json(createdChild);
+              .status(201)
+              .json(createdChild);
         } )
-        .catch((err) => {
+        .catch((err) =>{
             res
-            .status(400)
-            .json(err)
-        } );
-} )
+              .status(400)
+              .json(err)
+        });
+})
+
+//GET '/api/child' returns all children for currently logged in teacher
+router.get('/child', (req, res, next) =>{
+    Child
+    .find()
+    .populate('events')
+    .then((allChildren) =>{
+        res
+          .status(200)
+          .json(allChildren);
+    })
+    .catch((err) =>{
+        res
+          .status(400)
+          .json(err)
+    });
+})
+
+//GET '/api/child/:id' shows specific child
+router.get('/child/:id', (req, res, next) =>{
+    const {id} = req.params;
+    Child
+        .findById(id)
+        .populate('events')
+        .then((foundChild) =>{
+            res
+              .status(200)
+              .json(foundChild);
+        })
+        .catch((err) =>{
+            res
+              .status(400)
+              .json(err)
+            });
+})
 
 module.exports = router;
